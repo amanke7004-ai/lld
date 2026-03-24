5d78b922edbb9fa095fe60c59b05db6e12de64e1
amankeshri001 <amankeshri001@gmail.com>
2026-03-24 12:36:44 +0530

    added code for thread safe token based rate limiter

---
+++ src/main/java/com/multithreading/ThreadSafeTokenRateLimiter.java
@@ -0,0 +1,60 @@
+package com.multithreading;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+class TokenBasedRateLimiter {
+    private int tokens;
+    private long lastRefillTime;
+    private int capacity;
+    private int refillRatePerSecond;
+    
+    public TokenBasedRateLimiter(int capacity, int refillRatePerSecond){
+        this.capacity = capacity;
+        this.tokens = capacity;
+        this.refillRatePerSecond = refillRatePerSecond;
+        this.lastRefillTime = System.currentTimeMillis();
+    }
+    
+    public synchronized String processRequest(){
+        refill();
+        if(this.tokens > 0){
+            this.tokens--;
+            return "Request Processed";
+        }else{
+            return "Request Rejected";
+        }
+    }
+    private void refill(){
+        int timeElapsedInSeconds = (int)(System.currentTimeMillis()-lastRefillTime/1000);
+        int tokensToAdd = 0;
+        if(timeElapsedInSeconds > 0){
+            tokensToAdd = timeElapsedInSeconds * refillRatePerSecond;
+        }
+        this.tokens = Math.min(capacity, this.tokens + tokensToAdd);
+        lastRefillTime = System.currentTimeMillis();
+    }
+}
+
+public class ThreadSafeTokenRateLimiter{
+
+    public static void main(String[] args) throws InterruptedException {
+        TokenBasedRateLimiter tokenBasedRateLimiter = new TokenBasedRateLimiter(4,1);
+        Runnable task = ()->{
+            String result = tokenBasedRateLimiter.processRequest();
+            System.out.println(result);
+        };
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        for(int i = 0; i <= 200; i++) {
+            executorService.execute(task);
+        }
+        executorService.shutdown();
+        boolean taskFinished = executorService.awaitTermination(10, TimeUnit.SECONDS);
+        if(!taskFinished){
+            System.out.println("All tasks not finished");
+        }
+    }
+}
+
+
